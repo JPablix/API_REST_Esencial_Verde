5922bd49a067f16fdeda68ba9fca47c0bee30954
Jose Granados <josepgs0929@gmail.com>
2023-05-13 02:53:20 -0600

    ADD: containers endpoint

---
+++ src/controllers/containers.controller.js
@@ -0,0 +1,27 @@
+import {getConnection, sql} from '../database/connection';
+import { queries } from '../database/querys';
+
+export const getContainers = async (req, res) => {
+    const pool = await getConnection();
+    const result = await pool.request().query(queries.getAllContainers);
+    console.log(result);
+
+    res.json(result.recordset);
+};
+
+
+export const getContainersByWaste = async (req, res) => {
+    const {id} = req.params;
+    try{
+        const pool = await getConnection()
+        const result = await pool.request()
+            .input('Id', sql.Int, id)
+            .query(queries.getContainersByWaste)
+        console.log(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+    res.send(id);
+}
+
